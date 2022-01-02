988e3fb608ecce7f2a79751098e8d66b60487941
Edward Wang <edwardw483@gmail.com>
2022-01-02 15:43:54 -0500

    can play rps

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
+playerScore = 0;
+computerScore = 0;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', playerPlay);
+});
+
 function computerPlay(){
     let rand = Math.floor(Math.random() * 3);
+    const image = document.querySelector(".moves .computer img");
     if(rand == 0){
+        image.setAttribute("src", "./images/rock.png");
         return "rock";
     }else if(rand == 1){
+        image.setAttribute("src", "./images/paper.png");
         return "paper";
     }else if(rand == 2){
+        image.setAttribute("src", "./images/scissors.png");
         return "scissors";
     }
 }
 
-function playerPlay(){
+function playerPlay(e){
     //return window.prompt("What move would you like to play?");
-    return "rock";
+    singleRound(this.id, computerPlay());
+    const image = document.querySelector(".moves .player img");
+    if(this.id == "rock"){
+        image.setAttribute("src", "./images/rock.png");
+    }else if(this.id == "paper"){
+        image.setAttribute("src", "./images/paper.png");
+    }else if(this.id == "scissors"){
+        image.setAttribute("src", "./images/scissors.png");
+    }
 }
 
 function singleRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    const pScore = document.querySelector(".player p");
+    const cScore = document.querySelector(".computer p");
     if(playerSelection == computerSelection){
-        return "Draw. Both players played " + playerSelection;
+        console.log("Draw. Both players played " + playerSelection);
     }else if(playerSelection == "rock" && computerSelection == "paper"){
-        return "You lose, " + computerSelection + " beats " + playerSelection; 
+        computerScore++;
+        console.log("You lose, " + computerSelection + " beats " + playerSelection); 
     }else if(playerSelection == "rock" && computerSelection == "scissors"){
-        return "You win, " + playerSelection + " beats " + computerSelection; 
+        playerScore++;
+        console.log("You win, " + playerSelection + " beats " + computerSelection); 
     }else if(playerSelection == "paper" && computerSelection == "rock"){
-        return "You win, " + playerSelection + " beats " + computerSelection;  
+        playerScore++;
+        console.log("You win, " + playerSelection + " beats " + computerSelection);  
     }else if(playerSelection == "paper" && computerSelection == "scissors"){
-        return "You lose, " + computerSelection + " beats " + playerSelection; 
+        computerScore++;
+        console.log("You lose, " + computerSelection + " beats " + playerSelection); 
     }else if(playerSelection == "scissors" && computerSelection == "paper"){
-        return "You win, " + playerSelection + " beats " + computerSelection;  
+        playerScore++;
+        console.log("You win, " + playerSelection + " beats " + computerSelection);  
     }else if(playerSelection == "scissors" && computerSelection == "rock"){
-        return "You lose, " + computerSelection + " beats " + playerSelection; 
+        computerScore++;
+        console.log("You lose, " + computerSelection + " beats " + playerSelection); 
     }else{
-        return "Not a valid move by player";
+        console.log("Not a valid move by player");
     }
+    pScore.textContent = `Player: ${playerScore}`;
+    cScore.textContent = `Computer: ${computerScore}`;
+    
+
 }
 
 function game(){
     for(let i = 0; i < 5; i++){
-        console.log(singleRound(playerPlay(), computerPlay()));
+        console.log("hello world");
     }
 }
 
